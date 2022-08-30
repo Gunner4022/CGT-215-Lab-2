1a5ca8c3f599a59005e4f895c3f51745cc9d9066
Noah Egierski <noahegierski@yahoo.com>
2022-08-30 18:01:14 -0400

    I finished the lab, or so I think. Woohoo

--- CGT 215 Lab 2/CGT 215 Lab 2.cpp
+++ CGT 215 Lab 2/CGT 215 Lab 2.cpp
@@ -1,28 +1,37 @@
-// CGT 215 Lab 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
 	float A;
 	float B;
-	float X;
+	float x;
+	/*cout << "Please enter a value for A: ";   //Asks for input value
+	cin >> A;                                   //Sets A to the user input
+	cout << "A=" << A << endl;                  //Takes user input and prints "A=(whatever user entered)" 
+	B = (A * 4) + 1;                            //Uses example equation to give B a value
+	cout << "And B=" << B << endl;              //Prints what B equals after the equation    */
+	cout << "Hello! Let's solve the equation:" << endl;
+	cout << "Ax + B = 0" << endl;
+	cout << "For the variable x" << endl;
+	cout << endl;
+
 	cout << "Please enter a value for A: ";
 	cin >> A;
-	cout << "A=" << A << endl;
-	B = (A * 4) + 1;
-	cout << "And B=" << B << endl;
+	cout << "Alright, so A=" << A << "." << endl;
+	cout << endl;
+
+	cout << "Now please enter a value for B: ";
+	cin >> B;
+	cout << "Nice, so now B=" << B << "." << endl;
+	cout << endl;
+
+	cout << "Now solving the equation: " << A << "x" << "+" << B << "=0..." << endl;
+
+	x = (0 - B) / A;
+
+	cout << " The variable x= " << x << " in this equation" << endl;
+
+
 }
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
